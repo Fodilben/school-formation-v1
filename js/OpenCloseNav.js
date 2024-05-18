8a6f0bdf364fa7a12070d09adafa829cdd245b46
Ayoub SADAOUI <almrftcom@gmail.com>
2024-05-18 00:41:31 -0700

    added css to the main section

--- js/OpenCloseNav.js
+++ js/OpenCloseNav.js
@@ -1,13 +1,11 @@
 function openNav() {
-  document.getElementById("nav-container").style.width = "25%";
-  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("nav-container").style.width = "300px";
   document.getElementById("openbtn").style.display = "none";
   document.getElementById("closebtn").style.display = "block";
 }
 
 function closeNav() {
   document.getElementById("nav-container").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
   document.getElementById("openbtn").style.display = "block";
   document.getElementById("closebtn").style.display = "none";
 }
